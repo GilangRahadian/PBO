98b724f09845ac2da0026b6cd8fef1fb74505f6a
GilangRahadian <115676033+GilangRahadian@users.noreply.github.com>
2022-10-13 19:41:50 +0700

    Add files via upload

---
+++ Setter/Setter/Program.cs
@@ -0,0 +1,23 @@
+﻿// Include namespace system
+using System;
+
+
+public class User
+{
+    public String username;
+    public String password;
+    public User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
+    }
+}
+public class DemoConstructor
+{
+    public static void Main(String[] args)
+    {
+        var petani = new User("petanikode", "kopi");
+        Console.WriteLine("Username: " + petani.username);
+        Console.WriteLine("Password: " + petani.password);
+    }
+}
